1224882222662289a796c407583ec2abd74a53e7
europeandreams <europeandreams01@gmail.com>
2026-03-17 16:34:12 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'QuickBook Helpline Guide'
+author = 'European Dreams'
+release = '1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
+
+html_favicon = '_static/favicon.png'
+
+html_title = "QuickBook Helpline Guide | Customer Support & Help"
